bf0fc2ed7286c6375cd6f1d8039a47becfcb9cf8
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-30 00:43:05 -0300

    cores background ajustadas

--- src/components/theme-button/index.tsx
+++ src/components/theme-button/index.tsx
@@ -2,7 +2,7 @@ import { IoMdMoon, IoIosSunny } from "react-icons/io";
 
 const ThemeButton = () => {
     return (
-        <div className="fixed top-2 right-5 flex gap-1 bg-amber-50 rounded-3xl border-5 border-amber-50 cursor-pointer">
+        <div className="fixed top-2 right-5 flex gap-1 bg-white rounded-3xl border-5 border-white cursor-pointer">
             <span className="absolute w-6 h-6 bg-green-domus rounded-[20px] border border-green-domus outline-3 outline-green-domus"></span>
             <IoIosSunny className="w-6 h-6 m-0.2 z-[3]" />
             <IoMdMoon className="w-6 h-6 m-0.2 z-[3]" />
