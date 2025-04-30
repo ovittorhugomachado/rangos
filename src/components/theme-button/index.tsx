2e5f7f454e835f40ecef24bbdc07ecbe2a9f908c
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-30 00:31:52 -0300

    cor do theme button ajustada

--- src/components/theme-button/index.tsx
+++ src/components/theme-button/index.tsx
@@ -2,8 +2,8 @@ import { IoMdMoon, IoIosSunny } from "react-icons/io";
 
 const ThemeButton = () => {
     return (
-        <div className="fixed top-2 right-5 flex gap-1 bg-gray-300 rounded-3xl border-5 border-gray-300 cursor-pointer">
-            <span className="absolute w-6 h-6 bg-green-400 rounded-[20px] border border-green-400 outline-3 outline-green-400"></span>
+        <div className="fixed top-2 right-5 flex gap-1 bg-amber-50 rounded-3xl border-5 border-amber-50 cursor-pointer">
+            <span className="absolute w-6 h-6 bg-green-domus rounded-[20px] border border-green-domus outline-3 outline-green-domus"></span>
             <IoIosSunny className="w-6 h-6 m-0.2 z-[3]" />
             <IoMdMoon className="w-6 h-6 m-0.2 z-[3]" />
         </div>
