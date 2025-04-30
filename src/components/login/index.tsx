ca2340ed79619ff142d86902266c3bf57bc5c3b3
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-30 00:36:46 -0300

    loginContainer com responsividade finalizada

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -3,7 +3,7 @@ import { FaArrowRight } from "react-icons/fa";
 const LoginContainer = () => {
     return (
         <>
-            <main className="bg-amber-50 border-1 border-zinc-400 rounded-xl w-120 pt-15 pb-25 p-5 flex flex-col justify-center items-center">
+            <main className="bg-amber-50 border-1 border-zinc-400 rounded-xl w-120 m-3 pt-15 pb-20 p-5 flex flex-col justify-center items-center">
                 <img className="w-35 ml-8 mb-3" src="../logo.png" alt="domus-logo" />
                 <div className="flex flex-col mt-[20px] mb-[20px] w-[100%] max-w-105 gap-1">
                     <label className="font-semibold" htmlFor="password">Email</label>
@@ -12,7 +12,7 @@ const LoginContainer = () => {
                     <input className="input" id="password" type="password" />
                     <a className="link text-end" href="#">Esqueci minha senha</a>
                 </div>
-                <a className="link" href="#">Ainda não tem conta? <strong>Criar conta <FaArrowRight className="inline" /></strong></a>
+                <a className="link" href="#">Ainda não tem conta? <strong className="whitespace-nowrap">Criar conta <FaArrowRight className="inline" /></strong></a>
             </main>
         </>
     )
