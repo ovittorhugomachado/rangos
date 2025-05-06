6733ac5706f90f963758325cef4de09bec31e976
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-05-06 13:35:37 -0300

    service de login criado com fetch

---
+++ src/services/auth-service.tsx
@@ -0,0 +1,22 @@
+interface LoginCredentials {
+    email: string;
+    password: string;
+}
+
+export const login = async (credentials: LoginCredentials) => {
+    const response = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Erro ao fazer login')
+    }
+
+    const data = await response.json();
+    return data;
+}
