55864fbc0f812be86f4c261067f212636c9f92ab
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-05-08 11:37:19 -0300

    componente input phone number criado

---
+++ src/components/register/inputs/input-phone-number/index.tsx
@@ -0,0 +1,45 @@
+import { Controller } from "react-hook-form";
+import { IMaskInput } from "react-imask"
+import { InputPhoneNumberProps } from "../../../../types/inputs.d"
+
+const InputPhoneNumber = ({
+    control,
+    initialValues = {},
+}: InputPhoneNumberProps ) => {
+    return (
+        <Controller
+        name="number"
+        control={control}
+        defaultValue={initialValues.number || ""}
+        rules={{
+            required: "Campo obrigatório",
+            pattern: {
+                value: /^\(\d{2}\) \d{5}-\d{4}$/,
+                message: "Digite no formato (99) 99999-9999",
+            },
+        }}
+        render={({ field, fieldState }) => (
+            <>
+                <label htmlFor="number" className="label">
+                    Celular *
+                    {fieldState.error && (
+                        <span className="span-error">
+                            {fieldState.error.message}
+                        </span>
+                    )}
+                </label>
+                <IMaskInput
+                    {...field}
+                    mask="(00) 00000-0000"
+                    placeholder="(99) 99999-9999"
+                    className={`input mb-2 ${fieldState.error ? 'input-error' : ''}`}
+                    onAccept={(value) => field.onChange(value)}
+                />
+            </>
+
+        )}
+    />
+    )
+}
+
+export { InputPhoneNumber }
