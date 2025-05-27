37fbf25bd36b59491b948a8b9e2c7a101d34d7f8
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-05-27 19:57:56 -0300

    arquivo de types com todos os tipos do db

--- src/types/account-input-types.d.tsx
+++ src/types/account-input-types.d.tsx
@@ -1,40 +1,40 @@
 import { UseFormRegister, UseFormWatch, UseFormClearErrors, FieldErrors, Control } from "react-hook-form";
-import { AccountFormData } from "./account-types.d";
+import { AccountData } from "./account-types.d";
 
 export interface InputNameProps {
-    register: UseFormRegister<AccountFormData>;
-    errors: FieldErrors<AccountFormData>;
-    clearErrors: UseFormClearErrors<AccountFormData>;
-    initialValues?: Partial<AccountFormData>;
+    register: UseFormRegister<AccountData>;
+    errors: FieldErrors<AccountData>;
+    clearErrors: UseFormClearErrors<AccountData>;
+    initialValues?: Partial<AccountData>;
 }
 
 export interface InputCNPJProps {
-    control: Control<AccountFormData>;
-    initialValues?: Partial<AccountFormData>;
+    control: Control<AccountData>;
+    initialValues?: Partial<AccountData>;
 }
 
 export interface InputCPFProps {
-    control: Control<AccountFormData>;
-    initialValues: Partial<AccountFormData>;
+    control: Control<AccountData>;
+    initialValues: Partial<AccountData>;
 }
 
 export interface InputPhoneNumberProps {
-    control: Control<AccountFormData>;
-    initialValues: Partial<AccountFormData>;
+    control: Control<AccountData>;
+    initialValues: Partial<AccountData>;
 }
 
 export interface InputEmailProps {
-    register: UseFormRegister<AccountFormData>;
-    errors: FieldErrors<AccountFormData>;
-    clearErrors: UseFormClearErrors<AccountFormData>;
-    initialValues?: Partial<AccountFormData>;
+    register: UseFormRegister<AccountData>;
+    errors: FieldErrors<AccountData>;
+    clearErrors: UseFormClearErrors<AccountData>;
+    initialValues?: Partial<AccountData>;
 }
 
 export interface InputPasswordProps {
-    register: UseFormRegister<AccountFormData>;
-    errors: FieldErrors<AccountFormData>;
-    clearErrors: UseFormClearErrors<AccountFormData>;
-    watch: UseFormWatch<AccountFormData>;
-    initialValues?: Partial<AccountFormData>;
+    register: UseFormRegister<AccountData>;
+    errors: FieldErrors<AccountData>;
+    clearErrors: UseFormClearErrors<AccountData>;
+    watch: UseFormWatch<AccountData>;
+    initialValues?: Partial<AccountData>;
 }
 
--- src/types/account-types.d.tsx
+++ src/types/account-types.d.tsx
@@ -1,20 +1,37 @@
-export interface AccountFormData {
-  restaurantName: string;
-  cnpj: string;
-  ownersName: string;
-  cpf: string;
-  phoneNumber: string;
+export interface AccountData {
+  pageStyle(pageStyle: unknown): unknown;
+  id: number;
   email: string;
   password: string;
+  restaurantName: string;
+  activeAccount: boolean;
+  plan: string;
+  cratedAt: Date;
+  cnpj: string;
+  cpf: string;
+  ownersName: string;
+  phoneNumber: string;
+  passwordResetToken: object;
+  storeCustomization: object;
+  openingHours: object;
+
+  primaryColor: string;
+  backgroungColor: string;
+  textColor: string;
+  textButtonColor: string;
+  logoUrl: string;
+  bannerUrl: string;
+
   token: string;
+
   message: string;
   error: unknown;
 }
 
 export interface AccountContainerProps {
-  onSubmit: (data: AccountFormData) => void;
+  onSubmit: (data: AccountData) => void;
   isLoading: boolean;
   message: string;
   error?: string;
-  initialValues?: Partial<AccountFormData>;
+  initialValues?: Partial<AccountData>;
 }
