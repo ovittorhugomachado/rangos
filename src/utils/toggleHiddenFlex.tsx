0411f77f4e473de2dbff181fa0f5ccfc0ac71b3a
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-27 14:53:13 -0300

    criada função alternar entre hidden e flex

---
+++ src/utils/toggleHiddenFlex.tsx
@@ -0,0 +1,7 @@
+export function toggleHiddenFlex(element: HTMLElement) {
+    if (element.classList.contains('hidden')) {
+        element.classList.replace('hidden', 'flex');
+    } else if (element.classList.contains('flex')) {
+        element.classList.replace('flex', 'hidden');
+    }
+};
