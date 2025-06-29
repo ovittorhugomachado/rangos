3f8463d8e976653d1b8d7cd650b1a3f757295f4e
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-29 11:32:39 -0300

    componente loading usando o hook useAppSetgtings

--- src/components/loading-component/index.tsx
+++ src/components/loading-component/index.tsx
@@ -1,8 +1,13 @@
+import { useAppSettings } from "../../hooks/use-app-settings"
+
 export const LoadingComponent = () => {
+
+    const { fontSize } = useAppSettings();
+
     return (
         <div className="flex flex-col items-center gap-2">
             <div className="w-12 h-12 border-2 border-primary border-t-0 animate-spin rounded-full"/>
-            <p className="text-white dark:text-white">Carregando</p>
+            <p className={`${fontSize} text-white dark:text-white`}>Carregando</p>
         </div>
 
     )
