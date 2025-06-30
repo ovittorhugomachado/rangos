1d63902cae80d7a9b13bec4ce1eba945646af396
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-30 12:02:38 -0300

    componente categorybuttons deletando categoria

--- src/components/store-page/by-store/category-buttons/index.tsx
+++ src/components/store-page/by-store/category-buttons/index.tsx
@@ -1,13 +1,15 @@
 import { Category } from "../../../../types/restaurante-data-types.d";
 import { IoCloseOutline } from "react-icons/io5";
+import { deleteCategoryService } from "../../../../services/menu-store";
 
 interface CategoryButtonsProps {
     categories: Category[];
+    setCategories: React.Dispatch<React.SetStateAction<Category[]>>;
     buttonColor?: string;
     textColor?: string;
 }
 
-const CategoryButtons = ({ categories, buttonColor, textColor }: CategoryButtonsProps) => {
+const CategoryButtons = ({ categories, setCategories, buttonColor, textColor }: CategoryButtonsProps) => {
     const handleScroll = (id: string) => {
         const section = document.getElementById(id);
         if (section) {
@@ -15,13 +17,22 @@ const CategoryButtons = ({ categories, buttonColor, textColor }: CategoryButtons
         }
     };
 
+    const deleteCategory = async (categoryId: number) => {
+        try {
+            await deleteCategoryService(categoryId);
+            setCategories(prev => prev.filter(cat => cat.id !== categoryId));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="w-full flex flex-wrap justify-center gap-4 p-2.5 my-3.5">
             {categories.map((category) => (
-                <div 
-                key={category.id} 
-                className={`relative flex-grow min-w-28 h-8 lg:h-10 px-3 rounded-3xl cursor-pointer transition-transform duration-200 text-${textColor}`}
-                style={{ backgroundColor: buttonColor ?? '' }}
+                <div
+                    key={category.id}
+                    className={`relative flex-grow min-w-28 h-8 lg:h-10 px-3 rounded-3xl cursor-pointer transition-transform duration-200 text-${textColor}`}
+                    style={{ backgroundColor: buttonColor ?? '' }}
                 >
                     <button
                         type="button"
@@ -30,7 +41,10 @@ const CategoryButtons = ({ categories, buttonColor, textColor }: CategoryButtons
                     >
                         {category.name}
                     </button>
-                    <button className="w-5 h-5 z-2 flex items-center justify-center bg-red-600 text-white rounded-full absolute top-[-21px] right-[5px] cursor-pointer hover:shadow-[0_0_8px_0px_rgba(255,0,0,0.7)] transition-all duration-200">
+                    <button
+                        className="w-5 h-5 z-2 flex items-center justify-center bg-red-600 text-white rounded-full absolute top-[-11px] left-[0] border-[1px] border-amber-50 cursor-pointer hover:shadow-[0_0_8px_0px_rgba(255,0,0,0.7)] transition-all duration-200"
+                        onClick={() => deleteCategory(category.id)}
+                    >
                         <IoCloseOutline className="text-lg" />
                     </button>
                 </div>
--- src/services/menu-store.tsx
+++ src/services/menu-store.tsx
@@ -14,4 +14,21 @@ export const getCategoriesStore = async () => {
     };
 
     return await response.json();
-}
+};
+
+export const deleteCategoryService = async (categoryId: number) => {
+    const response = await fetch(`http://localhost:3000/categories/${categoryId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include'
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message || 'Erro ao deletar categoria do menu');
+    };
+
+    return await response.json();
+};
