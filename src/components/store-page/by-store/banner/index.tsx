4da6bbe2eb7b0d0e4a4cce2876fec00f2ce60329
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-02 14:54:15 -0300

    banner com imagem default

--- src/components/store-page/by-store/banner/index.tsx
+++ src/components/store-page/by-store/banner/index.tsx
@@ -22,28 +22,27 @@ const StoreBanner = ({ banner, onBannerChange }: { banner: string, onBannerChang
                 onBannerChange();
             } catch (error) {
                 console.error("Erro ao enviar imagem:", error);
-                setError("Erro ao enviar imagem. Por favor, tente novamente.");
+                setError("Erro ao enviar imagem. Por favor, tente novamente mais tarde.");
             }
         }
     };
 
     return (
         <div className="relative hidden xs:block">
-            <img src={banner ? `${banner}?v=${bannerVersion}` : ""} alt="imagem-capa" className="w-screen h-40 md:h-56 lg:h-72 object-cover" />
-            <button
-                type="button"
-                className="rounded-full absolute bottom-2 left-2 border-2 bg-white bg-opacity-40 border-black cursor-pointer px-4 py-1 hover:scale-105 transition-all duration-200"
-                onClick={handleButtonClick}
-            >
-
-                <span className="flex items-center justify-center gap-1 text-xs text-zinc-700">
+            <img src={banner ? `${banner}?v=${bannerVersion}` : "/store-banner-default.png"} alt="imagem-capa" className="w-screen h-fu md:h-56 lg:h-72 object-cover" />
+            <div className="flex items-center gap-2 absolute bottom-2 left-2">
+                <button
+                    type="button"
+                    className="rounded-full flex items-center justify-center w-[35px] h-[35px] border-2 bg-white bg-opacity-40 border-black cursor-pointer hover:scale-105 transition-all duration-200"
+                    onClick={handleButtonClick}
+                >
                     <FaCamera className="text-black" />
-                    Alterar imagem do banner
-                </span>
-                <span className=" text-[10px] text-zinc-700 px-2 py-1 shadow">
+                </button>
+                <span className="h-5 hidden sm:flex items-center text-[10px] bg-white rounded-full text-zinc-700 px-2 py-1 shadow">
                     *Recomenda-se 2000x527px
                 </span>
-            </button>
+            </div>
+
             <input
                 type="file"
                 accept="image/*"
