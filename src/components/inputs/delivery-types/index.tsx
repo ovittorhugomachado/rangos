9c871bb4e7b79cb2fe6fab52f78502243e3c2119
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-03 03:50:18 -0300

    criado input de tipos de entrega

---
+++ src/components/inputs/delivery-types/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { UseFormRegister } from "react-hook-form";
+import { AccountData } from "../../../types/account-types.d";
+
+interface UpdateDeliveryTypesInputProps {
+    register: UseFormRegister<AccountData>;
+}
+
+export const CheckboxDeliveryTypesInput = ({
+    register,
+}: UpdateDeliveryTypesInputProps) => (
+    <div className="flex items-center mx-auto gap-4 mt-2">
+        <span className="font-bold">Tipos de entrega:</span>
+        <label htmlFor="delivery">Delivery</label>
+        <input
+            type="checkbox"
+            id="delivery"
+            {...register("delivery")}
+        />
+        <label htmlFor="pickup">Retirada</label>
+        <input
+            type="checkbox"
+            id="pickup"
+            {...register("pickup")}
+        />
+    </div>
+);
