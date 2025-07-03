65d83aa97d9053c2687860edf5b63b02c25a2203
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-03 03:28:58 -0300

    input de endereço criado

---
+++ src/components/inputs/input-address/index.tsx
@@ -0,0 +1,42 @@
+import { InputAddressProps } from "../../../types/account-input-types.d";
+
+const InputAddress = ({
+    register, 
+    errors,
+    clearErrors,
+    initialValues = {}, 
+}: InputAddressProps) => {
+
+    return (
+        <>
+            <label htmlFor="address" className="label">
+                Endereço
+                {errors.address && (
+                    <span className="span-error">
+                        {errors.address.message?.toString()}
+                    </span>
+                )}
+            </label>
+            <input
+                id="address"
+                type="text"
+                className={`input mb-2 ${errors.address ? "input-error" : ""}`}
+                placeholder="Restaurante"
+                defaultValue={initialValues.address || ""}
+                {...register("address", {
+                    minLength: {
+                        value: 2,
+                        message: "Campo obrigatório"
+                    },
+                    onChange: (e) => {
+                        if (e.target.value.length > 4) {
+                            clearErrors("address");
+                        }
+                    }
+                })}
+            />
+        </>
+    )
+}
+
+export { InputAddress }
