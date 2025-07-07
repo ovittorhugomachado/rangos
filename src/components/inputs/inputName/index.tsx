6dc6a27e623902e572e65d465d56e76e8866e14b
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-07 20:17:36 -0300

    criado inputname

---
+++ src/components/inputs/inputName/index.tsx
@@ -0,0 +1,42 @@
+import { InputMenuItemNameProps } from "../../../types/account-input-types.d";
+
+const InputName = ({
+    register,
+    errors,
+    clearErrors,
+    initialValues = {},
+}: InputMenuItemNameProps) => {
+    return (
+        <>
+            <label htmlFor="name" className="label">
+                Nome *
+                {errors.name && (
+                    <span className="span-error">
+                        {errors.name.message?.toString()}
+                    </span>
+                )}
+            </label>
+            <input
+                id="name"
+                type="text"
+                className={`input mb-2 ${errors.name ? "input-error" : ""} autofill:text-black`}
+                placeholder="Nome"
+                defaultValue={initialValues.name || ""}
+                {...register("name", {
+                    required: "Campo obrigatório",
+                    minLength: {
+                        value: 2,
+                        message: "Campo obrigatório"
+                    },
+                    onChange: (e) => {
+                        if (e.target.value.length > 4) {
+                            clearErrors("name");
+                        }
+                    }
+                })}
+            />
+        </>
+    )
+}
+
+export { InputName }
