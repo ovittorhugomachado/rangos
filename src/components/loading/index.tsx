cc663607d108aab3ecf74d976626d79b00a561b0
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-07 13:51:51 -0300

    loafing com letra branco no tema dark

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -7,7 +7,7 @@ export const LoadingComponent = () => {
     return (
         <div className="flex flex-col items-center gap-2">
             <div className="w-12 h-12 border-2 border-primary border-t-0 animate-spin rounded-full"/>
-            <p className={`${fontSize} text-white dark:text-white`}>Carregando</p>
+            <p className={`${fontSize} dark:text-white`}>Carregando</p>
         </div>
 
     )
