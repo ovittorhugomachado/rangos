2605b68f50133abd15ad71fd8b34668da1f73a77
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-11 14:54:41 -0300

    dashboard cards com fontSize dinamica

--- src/components/dashboard-cards/index.tsx
+++ src/components/dashboard-cards/index.tsx
@@ -1,26 +1,29 @@
 import { IoIosArrowDown } from "react-icons/io"
 import { CountdownTimer } from "../countdown-timer"
-import { useState } from "react";
-import { useAppSettings } from "../../hooks/use-app-settings";
 import { toMoney } from "../../utils/transform-to-money";
 import { Order } from "../../types/orders-types.d";
 
 type DashboardCardProps = {
+    fontSize: string;
     orders: Order[];
+    activePanel: number[];
+    setActivePanel: React.Dispatch<React.SetStateAction<number[]>>;
     onAcceptOrder: (orderId: number) => void;
     onCancelOrder: (orderId: number) => void;
     onOrderReady: (orderId: number) => void;
     onOrderDelivered: (orderId: number) => void;
 };
 
-export const DashboardCards = ({ orders, onAcceptOrder, onCancelOrder, onOrderReady, onOrderDelivered }: DashboardCardProps) => {
-
-    const {
-        fontSize,
-    } = useAppSettings();
-
-
-    const [activePanel, setActivePanel] = useState<number[]>([]);
+export const DashboardCards = ({
+    fontSize,
+    orders,
+    activePanel,
+    setActivePanel,
+    onAcceptOrder,
+    onCancelOrder,
+    onOrderReady,
+    onOrderDelivered
+}: DashboardCardProps) => {
 
     const cards = [
         {
@@ -53,7 +56,7 @@ export const DashboardCards = ({ orders, onAcceptOrder, onCancelOrder, onOrderRe
             status: "entregue",
             color: "bg-green-800",
             titleColor: "text-white",
-            textColor: "text-black"
+            textColor: "text-zinc-300"
         },
         {
             id: 4,
@@ -61,7 +64,7 @@ export const DashboardCards = ({ orders, onAcceptOrder, onCancelOrder, onOrderRe
             status: ["cancelado", "cancelado_automaticamente"],
             color: "bg-red-600",
             titleColor: "text-white",
-            textColor: "text-black"
+            textColor: "text-zinc-300"
         },
     ];
 
@@ -75,7 +78,7 @@ export const DashboardCards = ({ orders, onAcceptOrder, onCancelOrder, onOrderRe
 
     return (
         <>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl mx-auto mt-46 sm:mt-26 w-full p-3.5 items-start">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl mx-auto w-full p-3.5 items-start">
                 {cards.map((card, index) => {
                     const cardOrders = Array.isArray(card.status)
                         ? orders.filter(order => card.status.includes(order.status))
@@ -91,11 +94,11 @@ export const DashboardCards = ({ orders, onAcceptOrder, onCancelOrder, onOrderRe
                                 <span className="w-7 h-7 rounded-full flex items-center justify-center bg-white dark:bg-black text-black dark:text-white text-sm">{cardOrders.length}</span>
                             </div>
                             {cardOrders.length === 0 ? (
-                                <div className={`${activePanel.includes(card.id) ? 'block' : 'hidden'} text-center py-4`}>Nenhum pedido</div>
+                                <p className={`${activePanel.includes(card.id) ? 'block' : 'hidden'} ${fontSize} text-center py-4`}>Nenhum pedido</p>
                             ) : (
                                 <div className={`${activePanel.includes(card.id) ? 'block' : 'hidden'} ${card.textColor} flex flex-col items-center px-4`}>
                                     {cardOrders.map((order, orderIndex) => (
-                                        <div key={orderIndex} className={`${fontSize} w-full flex flex-col items-start gap-2 px-2.5 py-2 border-y-[1px] border-white`}>
+                                        <div key={orderIndex} className={`${fontSize} w-full relative flex flex-col items-start gap-2 px-2.5 pt-4 py-2 border-y-[1px] border-white`}>
                                             <h1 className="text-center mx-auto px-2 text-lg font-bold border-b-2 border-black">{order.customerName}</h1>
                                             {card.name === "Aguardando aprovação" && (
                                                 <CountdownTimer createdAt={order.createdAt} durationSeconds={600} />
@@ -128,7 +131,7 @@ export const DashboardCards = ({ orders, onAcceptOrder, onCancelOrder, onOrderRe
                                                     <button className={`${fontSize} bg-red-600 text-white px-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-300`} onClick={() => onCancelOrder(order.id)}>Cancelar pedido</button>
                                                 </div>
                                             )}
-                                            {Array.isArray(card.status) && card.status.includes(order.status) && card.name !== "Cancelado" && (
+                                            {Array.isArray(card.status) && card.status.includes(order.status) && card.name !== "Cancelados" && (
                                                 <div className="w-full flex justify-center gap-3 mt-2 mb-1">
                                                     <button className={`${fontSize} bg-green-600 border-2 border-green-900 text-white px-2 py-1 rounded-full cursor-pointer hover:scale-105 transition-all duration-300`} onClick={() => onOrderDelivered(order.id)}>Pedido entregue</button>
                                                     <button className={`${fontSize} bg-red-600 text-white px-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-300`} onClick={() => onCancelOrder(order.id)}>Cancelar pedido</button>
