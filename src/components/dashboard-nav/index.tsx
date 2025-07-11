179ce032e6d20b42ecef650af2ce8f8f04f9d809
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-11 15:10:40 -0300

    ajustada responsividade do van

--- src/components/dashboard-nav/index.tsx
+++ src/components/dashboard-nav/index.tsx
@@ -1,23 +1,35 @@
 import { Logo } from "../logo"
-import { CgMenuGridR } from "react-icons/cg"
 import { Link } from "react-router-dom"
+import { CgMenuGridR } from "react-icons/cg"
 import { ImExit } from "react-icons/im"
+import { LiaPaintBrushSolid } from "react-icons/lia";
 
 export const DashboardNav = () => {
     return (
         <>
             <nav className="w-screen h-54 sm:gap-36 absolute top-0 flex flex-col sm:flex-row sm:h-26 items-center justify-center py-12 px-4">
-                <div className="container-logo sm:absolute sm:left-8 mt-2 mb-4">
+                <div className="container-logo sm:absolute md:left-8 mt-2 mb-4">
                     <Logo />
                 </div>
-                <ul className="w-full sm:w-[330px] lg:w-[400px] flex flex-col sm:flex-row sm:translate-x-20 lg:translate-x-0 items-center justify-center gap-2 sm:gap-8 sm:mt-0 sm:justify-around text-black dark:text-white">
+                <ul className="w-full sm:w-[490px] lg:w-[550px] flex flex-col sm:flex-row md:translate-x-20 lg:translate-x-0 items-center justify-center gap-2 sm:gap-8 sm:mt-18 md:mt-0 md:justify-around text-black dark:text-white">
+                    
+                    <li className="flex justify-center border-b-2 border-primary sm:px-4">
+                        <Link
+                            to="/"
+                            className="text-center text-lg flex items-center gap-2 hover:scale-103 transition-all duration-200"
+                            title="Editar cardápio"
+                        >
+                            <CgMenuGridR />
+                            Painel
+                        </Link>
+                    </li>
                     <li className="flex justify-center sm:border-b-2 sm:border-primary sm:px-4">
                         <Link
                             to="/personalizar-cardapio"
                             className="text-center text-lg flex items-center gap-2 hover:scale-103 transition-all duration-200"
                             title="Editar cardápio"
                         >
-                            <CgMenuGridR />
+                            <LiaPaintBrushSolid />
                             Editar cardápio
                         </Link>
                     </li>
