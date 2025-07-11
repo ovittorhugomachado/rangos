06a5ed5457aa3bdbe1b9b24b470a5b21bb713f1f
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-11 15:11:47 -0300

    criado services de gerenciar pedidos

---
+++ src/services/manage-orders.ts
@@ -0,0 +1,127 @@
+import { Order } from "../types/orders-types.d";
+
+export const getOrdersService = async (limit: number = 1000, offset: number = 0): Promise<Order[]> => {
+    try {
+        const response = await fetch(`http://localhost:3000/order/list?limit=${limit}&offset=${offset}`, {
+            credentials: 'include',
+        });
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error("Erro ao buscar pedidos:", error);
+        return [];
+    }
+};
+
+export const createOrderService = async (orderData: Order) => {
+    try {
+        const response = await fetch('http://localhost:3000/order/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(orderData),
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao criar pedido');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao criar pedido:', error);
+        throw error;
+    }
+}; //FALTA TESTAR
+
+export const cancelOrderService = async (orderId: number) => {
+    try {
+        const response = await fetch(`http://localhost:3000/order/${orderId}/cancel`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao cancelar pedido');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao cancelar pedido:', error);
+        throw error;
+    }
+};
+export const acceptOrderService = async (orderId: number) => {
+    try {
+        const response = await fetch(`http://localhost:3000/order/${orderId}/accept`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao aceitar pedido');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao aceitar pedido:', error);
+        throw error;
+    }
+};
+
+export const readyOrderService = async (orderId: number) => {
+    try {
+        const response = await fetch(`http://localhost:3000/order/${orderId}/ready`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao atalizar status do pedido');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao atalizar status do pedido:', error);
+        throw error;
+    }
+};
+
+export const deliveredOrderService = async (orderId: number) => {
+    try {
+        const response = await fetch(`http://localhost:3000/order/${orderId}/delivered`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao atalizar status do pedido');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao atalizar status do pedido:', error);
+        throw error;
+    }
+};
