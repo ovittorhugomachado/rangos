e04703c126116af7f28440aa5ffc4da35ea14cfb
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-13 15:32:00 -0300

    finalizado footer by custoemr

---
+++ src/components/store-page/by-customer/footer/index.tsx
@@ -0,0 +1,11 @@
+export const StoreFooterComponent = ({ backgroundColor }: { backgroundColor: string }) => {
+    return (
+        <footer
+            style={{ backgroundColor: backgroundColor }}
+            className="w-screen h-[110px] flex items-center justify-center"
+        >
+            <h3 className={`${backgroundColor === 'black' ? 'text-white' : 'text-black'} absolute translate-x-[-95px] translate-y-[-20px]`}>by</h3>
+            <img className="w-40" src={backgroundColor === 'black' ? "../logo-dark.png" : "../logo.png"} alt="domus-logo" />
+        </footer>
+    );
+};    
