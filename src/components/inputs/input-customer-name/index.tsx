f5fe0ba8da6dd0133cfb399ad643b183b6c720c0
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-15 12:06:00 -0300

    criado input customer name

---
+++ src/components/inputs/input-customer-name/index.tsx
@@ -0,0 +1,40 @@
+import { InputCustomerNameProps } from "../../../types/account-input-types.d";
+
+export const InputName = ({
+    register,
+    errors,
+    clearErrors,
+    initialValues = {},
+}: InputCustomerNameProps) => {
+    return (
+        <>
+            <label htmlFor="name" className="label">
+                Nome *
+                {errors.customerName && (
+                    <span className="span-error">
+                        {errors.customerName.message?.toString()}
+                    </span>
+                )}
+            </label>
+            <input
+                id="name"
+                type="text"
+                className={`input mb-2 ${errors.customerName ? "input-error" : ""} autofill:text-black`}
+                placeholder="Nome"
+                defaultValue={initialValues.customerName || ""}
+                {...register("customerName", {
+                    required: "Nome do cliente obrigatório",
+                    minLength: {
+                        value: 2,
+                        message: "Nome do cliente obrigatório"
+                    },
+                    onChange: (e) => {
+                        if (e.target.value.length > 4) {
+                            clearErrors("customerName");
+                        }
+                    }
+                })}
+            />
+        </>
+    )
+};
