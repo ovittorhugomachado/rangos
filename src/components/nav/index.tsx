4778ceae294e71073c28a7e788337e570b42367d
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-15 20:21:02 -0300

    componente nav refatorado

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,51 +1,60 @@
-import { Logo } from "../logo"
-import { Link } from "react-router-dom"
-import { CgMenuGridR } from "react-icons/cg"
-import { ImExit } from "react-icons/im"
-import { LiaPaintBrushSolid } from "react-icons/lia";
+import { Link } from "react-router-dom";
+import { Logo } from "../logo";
+import { CgMenuGridR } from "react-icons/cg";
+import { ImExit } from "react-icons/im";
+import { MdBorderColor } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
 
 export const DashboardNav = () => {
-    return (
-        <>
-            <nav className="w-screen h-54 sm:gap-36 absolute top-0 flex flex-col sm:flex-row sm:h-26 items-center justify-center py-12 px-4">
-                <div className="container-logo sm:absolute md:left-8 mt-2 mb-4">
-                    <Logo />
-                </div>
-                <ul className="w-full sm:w-[490px] lg:w-[550px] flex flex-col sm:flex-row md:translate-x-20 lg:translate-x-0 items-center justify-center gap-2 sm:gap-8 sm:mt-18 md:mt-0 md:justify-around text-black dark:text-white">
-                    
-                    <li className="flex justify-center border-b-2 border-primary sm:px-4">
-                        <Link
-                            to="/"
-                            className="text-center text-lg flex items-center gap-2 hover:scale-103 transition-all duration-200"
-                            title="Editar cardápio"
-                        >
-                            <CgMenuGridR />
-                            Painel
-                        </Link>
-                    </li>
-                    <li className="flex justify-center sm:border-b-2 sm:border-primary sm:px-4">
-                        <Link
-                            to="/personalizar-cardapio"
-                            className="text-center text-lg flex items-center gap-2 hover:scale-103 transition-all duration-200"
-                            title="Editar cardápio"
-                        >
-                            <LiaPaintBrushSolid />
-                            Editar loja
-                        </Link>
-                    </li>
-                    <li className="flex justify-center sm:border-b-2 border-primary sm:px-4">
-                        <Link
-                            to="/entrar"
-                            className="text-center text-lg flex items-center gap-2 border-t-2 border-primary p-2 sm:p-0 sm:border-0  hover:scale-103 transition-all duration-200"
-                            title="Sair"
-                            onClick={() => localStorage.removeItem('token')}
-                        >
-                            <ImExit />
-                            Sair
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </>
-    )
+  return (
+    <nav className="absolute top-0 w-screen h-70 sm:h-38 lg:h-26 flex flex-col sm:flex-row items-center justify-center px-4 pt-16 sm:pt-12 py-12 lg:py-0 sm:gap-36 border-b border-zinc-800 dark:border-zinc-700">
+      <div className="container-logo mt-0 sm:absolute sm:mt-[-35px] lg:mt-0 lg:left-8 lg:translate-y-1">
+        <Logo />
+      </div>
+      <ul className="w-full md:w-[690px] flex flex-col sm:flex-row items-center justify-center md:justify-around gap-2 sm:gap-8 sm:mt-12 lg:mt-0 translate-y-3.5 lg:translate-y-0 xl:translate-x-12 lg:translate-x-20 text-black dark:text-white">
+        <li className="flex justify-center border-b-2 border-primary sm:px-4">
+          <Link
+            to="/"
+            title="Editar cardápio"
+            className="flex items-center justify-center gap-2 text-lg text-center text-black dark:text-white transition-all duration-200 hover:scale-103"
+          >
+            <CgMenuGridR />
+            Painel
+          </Link>
+        </li>
+        <li className="flex justify-center border-b-2 border-primary sm:px-4">
+          <Link
+            to="/store"
+            target="_blank"
+            title="Criar pedido"
+            className="flex items-center justify-center gap-2 text-lg text-center text-black dark:text-white transition-all duration-200 hover:scale-103"
+          >
+            <MdBorderColor />
+            Criar pedido
+          </Link>
+        </li>
+        <li className="flex justify-center sm:border-b-2 sm:border-primary sm:px-4">
+          <Link
+            to="/personalizar-cardapio"
+            title="Editar loja"
+            className="flex items-center justify-center gap-2 text-lg text-center text-black dark:text-white transition-all duration-200 hover:scale-103"
+          >
+            <IoMdSettings />
+            Editar loja
+          </Link>
+        </li>
+        <li className="flex justify-center border-t-2 border-primary p-2 sm:p-0 sm:border-0 sm:border-b-2 sm:border-primary sm:px-4">
+          <Link
+            to="/entrar"
+            title="Sair"
+            onClick={() => localStorage.removeItem("token")}
+            className="flex items-center justify-center gap-2 text-lg text-center text-black dark:text-white transition-all duration-200 hover:scale-103"
+          >
+            <ImExit />
+            Sair
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
