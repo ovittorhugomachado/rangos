019eb7c4b958d6bf3ddbea231b24fb4b8d9fc933
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-17 12:55:46 -0300

    criado componente restaruans grid

---
+++ src/components/customer-side/restaurants-grid.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { RestaurantsGrid } from "../../types/types-restaurante-data.d";
+
+export const StoreGrid = ({ restaurants }: RestaurantsGrid) => {
+
+    return (
+        <main className="w-full mx-4 md:mx-10 lg:mx-20 flex flex-col bg-white text-black">
+            <h1>Restaurantes</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 w-full mt-8">
+                {restaurants.map(restaurant => (
+                    <Link
+                        key={restaurant.id}
+                        to={`/restaurant/${restaurant.id}`}
+                        className="min-w-65 rounded-lg shadow p-4 border-[1px] border-zinc-300 flex flex-col items-center cursor-pointer hover:scale-103 transition-all duration-300">
+                        <img
+                            src={restaurant.logoUrl || "/store-logo-default.png"}
+                            alt={restaurant.name}
+                            className="w-24 h-24 object-cover rounded-full"
+                        />
+                            <h2 className="text-md font-semibold">{restaurant.restaurantName}</h2>
+                    </Link>
+                ))}
+            </div>
+        </main>
+    );
+};
