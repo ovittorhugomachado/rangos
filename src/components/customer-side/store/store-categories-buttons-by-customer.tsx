593fad80a43dd969d459266f40b932081d36ee62
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-18 11:05:03 -0300

    cor do texto dos botoes categorias direto no atributo style

--- src/components/customer-side/store/store-categories-buttons-by-customer.tsx
+++ src/components/customer-side/store/store-categories-buttons-by-customer.tsx
@@ -11,14 +11,16 @@ export const CategoryButtons = ({ categories, buttonColor, textColor }: Category
         }
     };
 
+    console.log(textColor)
+
     return (
         <div className="w-full flex flex-wrap justify-center gap-4 p-2.5 my-3.5 lg:text-base">
             {categories.map((category) => (
                 <button
                     key={category.id}
                     type="button"
-                    style={{ backgroundColor: buttonColor ?? '#a6a6a6' }}
-                    className={`text-${textColor ?? 'black'} relative flex-grow min-w-28 h-8 lg:h-10 px-3 rounded-3xl cursor-pointer transition-transform duration-200 hover:scale-101`}
+                    style={{ backgroundColor: buttonColor ?? '#a6a6a6', color: textColor ?? '#000' }}
+                    className={` relative flex-grow min-w-28 h-8 lg:h-10 px-3 rounded-3xl cursor-pointer transition-transform duration-200 hover:scale-101`}
                     onClick={() => handleScroll(`category-${category.id}`)}
                 >
                     {category.name}
