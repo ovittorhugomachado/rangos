ac1cafa65c6a296d0e4d25f9ecca34d060ba58ad
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-07-23 13:45:45 -0300

    banner by-customer com h-86

--- src/components/customer-side/store/store-banner-customer.tsx
+++ src/components/customer-side/store/store-banner-customer.tsx
@@ -1,7 +1,7 @@
 export const StoreBanner = ({ banner }: { banner: string }) => {
     
     return (
-        <div className="w-full h-full relative hidden xs:block">
+        <div className="w-full h-86 relative hidden xs:block">
             <img src={banner}
                 alt="banner-restaurante"
                 className="w-screen h-full object-cover" />
